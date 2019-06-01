aa949efdbea0e4e38e4be82349f274e4cd8923ef
bjccarter <40154768+bjccarter@users.noreply.github.com>
2019-06-01 13:47:23 -0400

    comit

--- WebApplication2/Controllers/ValuesController.cs
+++ WebApplication2/Controllers/ValuesController.cs
@@ -14,7 +14,7 @@ namespace WebApplication2.Controllers
         [HttpGet]
         public ActionResult<IEnumerable<string>> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "ben", "carter" };
         }
 
         // GET api/values/5
